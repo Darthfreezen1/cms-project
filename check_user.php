63cf7ed411f927256a112cfba97a351a81bd56e2
Darthfreezen1 <garettf999@gmail.com>
2019-03-16 14:12:13 +0000

    check user added

--- check_user.php
+++ check_user.php
@@ -1,9 +1,29 @@
 <?php
 
+if($_POST && !empty($_POST['username']) && !empty($_POST['password'])){
+    
+    require('connect.php');
 
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+    $query = "SELECT password FROM users WHERE username = :username";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':username', $username);
 
+    $statement->execute();
 
-header("Location: login.php?error");
+    $results = $statement->fetch(PDO::FETCH_ASSOC);
 
+    if(password_verify($password, $results['password'])){
+        header("Location: create_user.php?success");
+        exit;
+    }else {
+        header("Location: create_user.php?fail".$results['password']);
+        exit;
+    }
+
+}else {
+    exit;
+}
 ?>
